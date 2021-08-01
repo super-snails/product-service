a20d2bdc04343b08b65cadda7041e37f85b026af
tsing <ld320321@163.com>
2021-08-01 19:08:32 +0800

    :sparkles: 代码规范

--- src/main/java/com/tsing/product/api/teambuilding/service/TeambuildingService.java
+++ src/main/java/com/tsing/product/api/teambuilding/service/TeambuildingService.java
@@ -18,8 +18,22 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface TeambuildingService extends IService<TeambuildingEntity> {
 
+    /**
+     * 分页获取团建游产品列表
+     * @Date 2021/8/1 7:00 下午
+     * @author Tsing
+     * @param pageParam 查询条件
+     * @return 团建游信息
+     */
     ResultData<ResultPageData<TeambuildingBoEntity>> pageList(TeambildingPage pageParam);
 
+    /**
+     * 团建游修改信息
+     * @Date 2021/8/1 7:01 下午
+     * @author Tsing
+     * @param param 团建游修改信息
+     * @return boolean false 修改失败 true 修改成功
+     */
     ResultData<Boolean> modify(TeambuildingUpdateRequest param);
 
 }
